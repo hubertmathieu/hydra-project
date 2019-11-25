cf80e2561cca87faf26217ac0ed672f1ac068b32
LudoB99 <ludovic.belzile@hotmail.fr>
2019-11-25 14:15:59 -0500

    Post function

--- routes/api/towers.js
+++ routes/api/towers.js
@@ -19,12 +19,13 @@ let towers = {
         name: 'tower 1',
         data: {
             temperature: '36C',
-            humidity: '346456456%',
+            humidity: '346456456',
             lastSpray: '11/11/2019 - 21:33:41',
             nextSpray: '11/11/2019 - 21:38:41'
         }
     }
 };
+
 
 router.get('/', (req, res) => {
     return res.send(Object.values(towers));
@@ -37,10 +38,12 @@ router.post('/test', (req, res) => {
     res.send("it worked")
 });
 
+router.get('/send', (req, res) => {
+
+});
+
 router.get('/:towerId', (req, res) => {
     return res.send(towers[req.params.towerId]);
 });
-
-
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
+let guigui = [{
+  id: 1,
+  iconName : 'faTemperatureLow',
+  title: "Temp.",
+  value: "23.5 °C"
+},
+  {
+    id: 2,
+    iconName : 'faTint',
+    title: "Humidité",
+    value: "45.64 %"
+  }];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  var  ph = req.body.ph;
+  var  temp = req.body.temp;
+  res.send( 'Receiving Ph : ' + ph + ' and temp : ' + temp);
+});
+
+router.get('/gui', function(req, res, next) {
+  res.send(guigui);
 });
 
 module.exports = router;
