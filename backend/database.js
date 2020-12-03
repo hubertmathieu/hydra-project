1745ea8e97118079775044c0e32c65d9a3b6cbec
matdan <matdangariepy@hotmail.com>
2020-12-03 09:57:26 -0500

    data formating

--- backend/database.js
+++ backend/database.js
@@ -239,7 +239,8 @@ exports.updateSensors = function(data, serreId, callback = null){
         if(err) throw err;
         db.db('hydra').collection('threshold').update({serreId: serreId}, {$set: data}, function (err, res){
             if(err) throw err;
-            if(callback)callback(res);
+            //if(callback)callback(res);
+            callback(sensorArrayToObject(data));
             db.close();
         });
     });
