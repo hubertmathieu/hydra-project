6a3da66a44f7a31b6bd9ab7a655e82d4f56620b1
BananaRaptor <arnaudthebergemandeville@gmail.com>
2020-12-03 08:55:05 -0500

    push basic dick

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,8 @@
 var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/hydra';
 
+var dict = require('/dictionnaryModule').dictionnary;
+
 exports.authentifyUser = function(userInfo, callback) {
     mongoClient.connect(url, function(err, db) {
         if (err) throw err;
@@ -189,7 +191,21 @@ exports.findDefaultThreshold = function (data , callback) {
     })
 };
 
+
+}
+
+function getChangesFormatted(newValues) {
+    var changes = [];
+    for (propertyName in newValues){
+        changes.push({dict[propertyName]: newValues.propertyName})
+        console.log(changeToFormatting(propertyName,newValues));
+        console.log(`${propertyName}: ${newValues[propertyName]}`);
+    }
+    return [{'a':10}];
+}
+
 exports.updateThreshold = function (thresholdId, newValues) {
+    newValues.changes = getChangesFormatted(newValues);
     mongoClient.connect(url, function (err, db) {
         if (err) throw err;
         db.db('hydra').collection('threshold').updateOne({_id: thresholdId}, {$set: newValues}, function (err, res) {
---
+++ dictionnaryModule.js
@@ -0,0 +1,37 @@
+var dictionnary = {
+    serreId: 'a',
+
+    temperature1: 'b',
+
+    temperature2: 'c',
+
+    temperature3: 'd',
+
+    humidityLowerTreshold1:'e',
+
+    humidityUpperTreshold1:'f',
+
+    humidity1: 'g',
+
+    humidity2:'h',
+
+    humidity3:'i',
+
+    humidityLowerTreshold2:'j',
+
+    humidityUpperTreshold2: 'k',
+
+    humidityLowerTreshold3: 'l',
+
+    humidityUpperTreshold3: 'n',
+
+    ph: 'n',
+
+    waterLevel1: 60,
+
+    waterLevel2: 90,
+
+    phTreshold: 6.0
+}
+
+module.exports = DictModule;
