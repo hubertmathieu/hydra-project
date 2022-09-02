5a848fa02f422c04967e6a27a86e9476915f3d0b
Xavier Mathieu <xavier2003@hotmail.fr>
2022-09-02 16:19:02 -0400

    Removed counter up

--- backend/public/javascripts/main.js
+++ backend/public/javascripts/main.js
@@ -141,12 +141,6 @@
     allowPageScroll:"vertical"
   });
 
-  // jQuery counterUp (used in Facts section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
