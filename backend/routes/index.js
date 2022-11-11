520cd9c194f2048a549cce0533cadfa2b4b26417
Ludovick31 <83981459+Ludovick31@users.noreply.github.com>
2022-11-11 15:53:07 -0500

    Update index.js
    get data from db and pass to view

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const piecesList = require('../pieces');
+const thresholdBroker = require('../models/brokers/thresholdBroker');
+const configBroker = require('../models/brokers/configBroker');
 const pageTitlePrefix = "Serre Aéroponique Flottante Connectée | ";
 
 router.get('/', function (req, res) {
@@ -10,9 +12,34 @@ router.get('/', function (req, res) {
     });
 });
 
-router.get('/statistiques', function (req, res) {
+router.get('/statistiques', async function (req, res) {
+    let treshold = await thresholdBroker.findAll();
+    let config = await configBroker.findConfig();
+    let calvette1 = {};
+    calvette1.status = "En fonction";
+    calvette1.temperature = config[0].temperature1;
+    calvette1.treshold = treshold[0].treshold1;
+    calvette1.humidity = config[0].humidity1;
+
+    let calvette2 = {};
+    calvette2.status = "En fonction";
+    calvette2.temperature = config[0].temperature2;
+    calvette2.treshold = treshold[0].treshold2;
+    calvette2.humidity = config[0].humidity2;
+    console.log(calvette2)
+
+    let calvette3 = {};
+    calvette3.status = "En fonction";
+    calvette3.temperature = config[0].temperature1;
+    calvette3.treshold = treshold[0].treshold3;
+    calvette3.humidity = config[0].humidity3;
+
+
     res.render("pages/statistiques", {
         pageTitle: pageTitlePrefix + "Statistiques",
+        calvette1: calvette1,
+        calvette2: calvette2,
+        calvette3: calvette3,
         path: "/statistiques",
     });
 });
