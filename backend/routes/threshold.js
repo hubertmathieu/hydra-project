572236bfa9b234970e8330f816edda6783cbddb1
Xavier Mathieu <xavier2003@hotmail.fr>
2022-11-14 16:11:04 -0500

    Revert "Fix: fixed api"
    This reverts commit 18be6429d31211beb7a0004119340773779890f8.

--- backend/routes/threshold.js
+++ backend/routes/threshold.js
@@ -4,8 +4,7 @@ const thresholdBroker = require("../models/brokers/thresholdBroker")
 
 router.get('/', async function (req, res, next) {
     let thresholds = await thresholdBroker.findAll();
-    let formattedThresholds = "{\"threshold1\":" + thresholds[0].threshold1 + ",\"threshold2\":" + thresholds[0].threshold2 + ",\"threshold3\":" + thresholds[0].threshold3 + ",\"humiditythreshold1\":" + thresholds[0].humiditythreshold1 + ",\"humiditythreshold2\":" + thresholds[0].humiditythreshold2 + ",\"humiditythreshold3\":" + thresholds[0].humiditythreshold3 + ",\"phThreshold\":" + thresholds[0].phThreshold + "}";
-    res.send(formattedThresholds);
+    res.send(thresholds[0]);
 });
 
 router.post('/', async function (req) {
