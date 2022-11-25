4e6cc71c3def62e2aeb2182f85a3bf32ff031454
Xavier Mathieu <xavier2003@hotmail.fr>
2022-11-25 15:07:27 -0500

    Feat: added validations for API

--- backend/models/brokers/thresholdBroker.js
+++ backend/models/brokers/thresholdBroker.js
@@ -7,8 +7,15 @@ exports.findAll = function () {
     });
 };
 
-exports.updateThresholds = function (thresholdId, threshold) {
+exports.updateThresholds = function (thresholdId, humidityMinThreshold1, humidityMaxThreshold1, humidityMinThreshold2, humidityMaxThreshold2, humidityMinThreshold3, humidityMaxThreshold3) {
     return query("thresholds", (collection) => {
-        return collection.updateOne({_id: ObjectId(thresholdId)}, {$set: threshold});
+        return collection.updateOne({_id: ObjectId(thresholdId)}, {
+            "humidityMinThreshold1": humidityMinThreshold1,
+            "humidityMaxThreshold1": humidityMaxThreshold1,
+            "humidityMinThreshold2": humidityMinThreshold2,
+            "humidityMaxThreshold2": humidityMaxThreshold2,
+            "humidityMinThreshold3": humidityMinThreshold3,
+            "humidityMaxThreshold3": humidityMaxThreshold3,
+        });
     });
 };
