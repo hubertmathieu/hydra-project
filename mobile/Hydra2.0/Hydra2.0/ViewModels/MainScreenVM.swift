f26be144dceb6e9f11ec391985f9a9e3db73d595
Xavier Mathieu <90921613+Xavier7071@users.noreply.github.com>
2022-12-05 15:00:39 -0500

    Update MainScreenVM.swift

--- mobile/Hydra2.0/Hydra2.0/ViewModels/MainScreenVM.swift
+++ mobile/Hydra2.0/Hydra2.0/ViewModels/MainScreenVM.swift
@@ -30,11 +30,12 @@ class MainScreenVM: ObservableObject {
     func callOpenWeatherApi() {
         let url = URL(
             string:
-                "https://api.openweathermap.org/data/2.5/weather?q=Saint-Ours,ca&units=metric&appid=0423f1d8ad5cbacd8a4f2a0776e44a71"
+                "https://api.openweathermap.org/data/2
+                .5/weather?q=Saint-Ours
+                ,ca&units=metric&appid=0423f1d8ad5cbacd8a4f2a0776e44a71"
         )!
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
-            //let dataString = String(data: data, encoding: .utf8)
             self.handleOpenWeatherRequest(data: data)
         }
         task.resume()
