6d3cc47b7597d570b2d2981d6e4b25bc505d6d29
Ludovick31 <83981459+Ludovick31@users.noreply.github.com>
2022-12-09 15:13:27 -0500

    rename value

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,21 +16,21 @@ router.get('/statistiques', async function (req, res) {
     let threshold = await thresholdBroker.findAll();
     let config = await configBroker.findConfig();
     let calvette1 = {};
-    calvette1.status = config[0].humidity1 < threshold[0].humidityMinThreshold1 ? "En Fonction" : "Hors d'usage";
+    calvette1.status = config[0].humidity1 < threshold[0].humidityMinThreshold1 ? "En fonction" : "Hors d'usage";
     calvette1.temperature = config[0].temperature1 == 999 ? 0 : config[0].temperature1;
     calvette1.minThreshold = threshold[0].humidityMinThreshold1 == 999 ? 0 : threshold[0].humidityMinThreshold1;
     calvette1.maxThreshold = threshold[0].humidityMaxThreshold1 == 999 ? 0 : threshold[0].humidityMaxThreshold1;
     calvette1.humidity = config[0].humidity1 == 999 ? 0 : config[0].humidity1;
 
     let calvette2 = {};
-    calvette2.status = config[0].humidity2 < threshold[0].humidityMinThreshold2 ? "En Fonction" : "Hors d'usage";
+    calvette2.status = config[0].humidity2 < threshold[0].humidityMinThreshold2 ? "En fonction" : "Hors d'usage";
     calvette2.temperature = config[0].temperature2 == 999 ? 0 : config[0].temperature2;
     calvette2.minThreshold = threshold[0].humidityMinThreshold2 == 999 ? 0 : threshold[0].humidityMinThreshold2;
     calvette2.maxThreshold = threshold[0].humidityMaxThreshold2 == 999 ? 0 : threshold[0].humidityMaxThreshold2;
     calvette2.humidity = config[0].humidity2 == 999 ? 0 : config[0].humidity2;
 
     let calvette3 = {};
-    calvette3.status = config[0].humidity3 < threshold[0].humidityMinThreshold3 ? "En Fonction" : "Hors d'usage";
+    calvette3.status = config[0].humidity3 < threshold[0].humidityMinThreshold3 ? "En fonction" : "Hors d'usage";
     calvette3.temperature = config[0].temperature1 == 999 ? 0 : config[0].temperature1;
     calvette3.minThreshold = threshold[0].humidityMinThreshold3 == 999 ? 0 : threshold[0].humidityMinThreshold3;
     calvette3.maxThreshold = threshold[0].humidityMaxThreshold3 == 999 ? 0 : threshold[0].humidityMaxThreshold3;
